06dbc784b2d69b76f863e2989695bbacddddcba4
kevintian4 <kevintian20@gmail.com>
2024-07-06 11:29:22 -0700

    replace similar path param with recommendations and added credits

--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,9 @@ export async function getItemDetails(endpoint, id) {
   let url = '';
   
   if (endpoint === 'movie') {
-    url = `https://api.themoviedb.org/3/movie/${id}?append_to_response=images%2Cvideos%2Creviews%2Csimilar&language=en-US`;
+    url = `https://api.themoviedb.org/3/movie/${id}}?append_to_response=images%2Cvideos%2Creviews%2Crecommendations%2Ccredits&language=en-US`;
   } else if (endpoint === 'tv') {
-    url = `https://api.themoviedb.org/3/tv/${id}?append_to_response=images%2Cvideos%2Creviews%2Csimilar&language=en-US`;
+    url = `https://api.themoviedb.org/3/tv/${id}?append_to_response=images%2Cvideos%2Creviews%2Crecommendations%2Ccredits&language=en-US`;  
   } else if (endpoint === 'person') {
     url = `https://api.themoviedb.org/3/person/${id}?append_to_response=combined_credits&language=en-US`;
   } else {
