844b59147871602036abbee63f8c67d4be4878a6
kevintian4 <kevintian20@gmail.com>
2024-07-06 12:24:03 -0700

    added rating badge

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import { getItemDetails } from '../services/api';
 import { useState, useEffect } from 'react';
 import { marked } from 'marked';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function MovieDetails() {
   const { id } = useParams();
@@ -27,43 +29,57 @@ function MovieDetails() {
       return `${hours}h ${minutes}m`;
     }
   }  
+  const runtime = formatRuntime(data.runtime);
+
+  function formatRating(rating) { 
+    if (rating === null || rating === undefined) {
+      return null;
+    }
+    return `${rating.toFixed(1)}/10`;
+  }
+  const rating = formatRating(data.vote_average);
 
   const trailer = data.videos.results.find(video => video.type === 'Trailer' && video.site === 'YouTube');
   const videoKey = trailer ? trailer.key : null;
 
   const reviews = data.reviews.results.length > 0 ? data.reviews.results : null;
-  console.log(reviews)
+
   return (
-    <div className="movie-details">
-      <header className="movie-header" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${data.backdrop_path})` }}>
-        <div className="movie-header-content">
+    <div id="movie-details">
+      <header id="movie-header" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${data.backdrop_path})` }}>
+        { rating && 
+          <div id="rating-badge">
+            <FontAwesomeIcon icon={faStar} style={{color: "#FFD43B",}} /> {rating}
+          </div> 
+        }
+        <div id="movie-header-content">
           <h1>{data.title}</h1>
-          <p className="tagline">{data.tagline}</p>
-          <div className="movie-info">
-            <div className="movie-genres">
+          <p className="show-tagline">{data.tagline}</p>
+          <div className="show-header-info">
+            <div className="show-genres">
               {data.genres.map((genre) => (
                 <span key={genre.id} className="genre">{genre.name}</span>
               ))}
             </div>
-            <div className="movie-details-extra">
+            <div className="show-extra-details">
               <span><strong>Release Date:</strong> {data.release_date}</span>
-              <span><strong>Runtime:</strong> {formatRuntime(data.runtime)} </span>
+              { runtime && <span><strong>Runtime:</strong> {formatRuntime(data.runtime)} </span> }
             </div>
           </div>
         </div>
       </header>
 
       <main>
-        <section className="movie-overview">
+        <section className="overview-section">
           <h2>Overview</h2>
-          <p>{data.overview}</p>
+          <p className="overview">{data.overview}</p>
         </section>
 
         {videoKey && (
-          <section className="movie-trailer">
+          <section className="movie-trailer-section">
             <h2>Trailer</h2>
             <iframe
-              className="movie-page-trailer"
+              id="movie-trailer"
               src={`https://www.youtube.com/embed/${videoKey}`}
               allowFullScreen
               title="Movie Trailer"
@@ -72,12 +88,12 @@ function MovieDetails() {
         )}
         
         {reviews && (
-          <section className="movie-reviews">
+          <section className="reviews-section">
             <h2>Reviews</h2>
             {reviews.map((review) => (
               <div key={review.id} className="review">
-                <h3 className="author">{review.author}</h3>
-                  <div className="review-content" 
+                <h4 className="author">{review.author}</h4>
+                <div className="review-content" 
                   dangerouslySetInnerHTML={{ __html: marked.parse(review.content.replace(/\n/g, '<br />')) }} 
                 />
               </div>
