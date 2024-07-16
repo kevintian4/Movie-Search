ca868eaf8ed40a92ac9232895f3d8b3fb4610931
kevintian4 <kevintian20@gmail.com>
2024-07-15 20:39:38 -0700

    person details known for show carousel

--- src/components/ShowsCarousel.js
+++ src/components/ShowsCarousel.js
@@ -1,16 +1,29 @@
 import { TVCard } from "./TVCard";
+import { MovieCard } from "./MovieCard";
 
 function ShowsCarousel({ credits_data }) {
-  // filter out duplicate credits and sort by popularity
-  const cast_credits = (credits_data.cast)
-  .filter((item, index, self) => self.findIndex(i => i.id === item.id) === index)
-  .sort((a, b) => b.popularity - a.popularity);
+  // Filter out duplicate credits and sort by popularity
+  const cast_credits = credits_data.cast
+    .filter((item, index, self) => self.findIndex(i => i.id === item.id) === index)
+    .sort((a, b) => b.popularity - a.popularity);
 
   return (
-    // <div className="shows-carousel">
-    //   <TVCard tv={cast_credits[0]}/>
-    // </div>
-    <></>
+    <div className="shows-carousel">
+      {cast_credits.length > 0 && 
+        <>
+          <h2 className="shows-carousel-header">Known For</h2>
+          <div className="shows-carousel-list">
+            {cast_credits.slice(0, 40).map((show) => (
+              show.media_type === "tv" ? (
+                <TVCard key={show.id} tv={show} />
+              ) : (
+                <MovieCard key={show.id} movie={show} />
+              )
+            ))}
+          </div>
+        </>
+      }
+    </div>
   );
 }
 
