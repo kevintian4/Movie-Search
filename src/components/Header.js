0495fb1e0771e99e65f58a6bb6338c629c1f262c
kevintian4 <kevintian20@gmail.com>
2024-07-15 18:30:26 -0700

    added search bar in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 function goHome(){
     window.location.href = '/';
 }
 
 function Header(){
+    const navigate = useNavigate();
+
+    const handleSearch = (query, results) => {
+        navigate(`/search?q=${encodeURIComponent(query)}`, { state: { results } });
+        window.location.href = `/search?q=${encodeURIComponent(query)}`;
+    };
+
     return (
         <header>
             <FontAwesomeIcon icon={faHouse} onClick={goHome} className="header-button" id="home-button"/>
+            <SearchBar onSearch={handleSearch} />
         </header>
     )
 }
